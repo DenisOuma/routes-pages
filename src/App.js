5a96b0487ea05b6092775b82f1530b866c907b42
Denis Ouma <dennisouma38@gmail.com>
2023-03-19 19:51:45 +0300

    Children props application

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
 import React from "react";
+import { Button } from "./componets/Button";
 
 export const App = () => {
-	return <div>App</div>;
+	return (
+		<div>
+			<div>
+				<Button>primary</Button>
+			</div>
+			<div>
+				<Button>Secondary</Button>
+			</div>
+			<div>
+				<Button>Success</Button>
+			</div>
+			<div>
+				<Button>Danger</Button>
+			</div>
+			<div>
+				<Button>Welcome</Button>
+			</div>
+		</div>
+	);
 };
