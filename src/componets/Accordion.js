7c565a9abb4a1eb5efc4c555de904b82708fa5af
Denis Ouma <dennisouma38@gmail.com>
2023-03-21 18:54:48 +0300

    Adding Icon point direction

--- src/componets/Accordion.js
+++ src/componets/Accordion.js
@@ -1,13 +1,21 @@
 import React, { useState } from "react";
-
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
 export const Accordion = ({ items }) => {
 	const [expandedIndex, setExpandedIndex] = useState(0);
 
+	const handleToggle = (index) => {
+		setExpandedIndex(index);
+	};
 	const renderedItems = items.map((item, index) => {
 		const isExpanded = index === expandedIndex;
+		const icon = (
+			<span>{isExpanded ? <GoChevronDown /> : <GoChevronLeft />}</span>
+		);
 		return (
 			<div key={index}>
-				<div onClick={() => setExpandedIndex(index)}>{item.label}</div>
+				<div onClick={() => handleToggle(index)}>
+					{item.label} {icon}
+				</div>
 				{isExpanded && <div>{item.content}</div>}
 			</div>
 		);
