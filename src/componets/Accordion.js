4dccea50721d6ea79d0b88840ee8a9a8a634a744
Denis Ouma <dennisouma38@gmail.com>
2023-03-21 16:49:08 +0300

    Updating the dom with rendering data

--- src/componets/Accordion.js
+++ src/componets/Accordion.js
@@ -1,5 +1,12 @@
 import React from "react";
 
-export const Accordion = () => {
-	return <div></div>;
+export const Accordion = ({ items }) => {
+	const renderedItems = items.map((item, idex) => (
+		<div key={idex}>
+			<div>{item.label}</div>
+			<div>{item.content}</div>
+		</div>
+	));
+	console.log(items);
+	return <div>{renderedItems}</div>;
 };
