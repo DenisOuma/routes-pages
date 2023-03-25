31c05dc47b8bc2c79b550a56d1a568dc08daf13b
Denis Ouma <dennisouma38@gmail.com>
2023-03-25 18:18:46 +0300

    Setting the styling for the optioncomponent

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { DropDown } from "./componets/DropDown";
 
 export const App = () => {
+	const [select, setSelect] = useState();
 	const options = [
 		{ lable: "red", value: "red" },
 		{ lable: "green", value: "green" },
 		{ lable: "blue", value: "blue" },
 	];
+
+	const handleSelect = (option) => {
+		setSelect(option);
+	};
+	console.log(select);
+
 	return (
 		<div>
-			<DropDown options={options} />
+			<DropDown
+				options={options}
+				value={select}
+				lable={"Select... test"}
+				onChange={handleSelect}
+			/>
 		</div>
 	);
 };
--- src/componets/DropDown.js
+++ src/componets/DropDown.js
@@ -1,20 +1,41 @@
 import React, { useState } from "react";
 
-export const DropDown = ({ options }) => {
+export const DropDown = ({ options, onChange, value, lable }) => {
 	const [isOpen, setIsOpen] = useState(false);
 
 	const handleClick = () => {
 		setIsOpen((currentIsOpen) => !currentIsOpen);
 	};
+	const handleOptionclick = (option) => {
+		setIsOpen((currentClose) => !currentClose);
+		onChange(option);
+	};
 
 	const renderedOptions = options.map((option) => {
-		return <div key={option.value}>{option.lable}</div>;
+		return (
+			<div
+				className="hover:bg-sky-100 rounded cursor-pointer p1"
+				onClick={() => handleOptionclick(option.value)}
+				key={option.value}
+			>
+				{option.lable}
+			</div>
+		);
 	});
 
 	return (
-		<div>
-			<div onClick={handleClick}>Select...</div>
-			{isOpen && renderedOptions}
+		<div className="w-48 relative">
+			<div
+				className="flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full"
+				onClick={handleClick}
+			>
+				{value?.lable || lable}
+			</div>
+			{isOpen && (
+				<div className="absolute top-full border rounded p-3 bg-white w-full">
+					{renderedOptions}
+				</div>
+			)}
 		</div>
 	);
 };
