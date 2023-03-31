eec3ece7d268f1749c4055a123b13be2aa969aac
Denis Ouma <dennisouma38@gmail.com>
2023-03-31 08:46:52 +0300

    routing and path update functionality

--- src/context/navigation.js
+++ src/context/navigation.js
@@ -1,12 +1,32 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 const NavigationContext = createContext();
 
 const NavigationProvider = ({ children }) => {
 	const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+	useEffect(() => {
+		const handler = () => {
+			setCurrentPath(window.location.pathname);
+		};
+
+		window.addEventListener("popstate", handler);
+		return () => {
+			window.removeEventListener("popstate", handler);
+		};
+	}, []);
+
+	const navigate = (to) => {
+		window.history.pushState({}, "", to);
+		setCurrentPath(to);
+	};
 	const stateToShare = {};
 
 	return (
 		<NavigationContext.Provider value={stateToShare}>
+			<div>
+				<button onClick={() => navigate("/accordion")}>Go to accordion</button>
+				<button onClick={() => navigate("/dropdown")}>Go to dropDown</button>
+			</div>
 			{currentPath}
 			{children}
 		</NavigationContext.Provider>
