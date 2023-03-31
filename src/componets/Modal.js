84a388b1c7fe961474da51dbce5d96a3cda5c002
Denis Ouma <dennisouma38@gmail.com>
2023-03-31 19:57:46 +0300

    updating the dialog component for the application

--- src/componets/Modal.js
+++ src/componets/Modal.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
-export const Modal = ({ handleClose }) => {
+export const Modal = ({ children, handleClose, actionBar }) => {
+	useEffect(() => {
+		document.body.classList.add("overflow-hidden");
+
+		// clean up function
+		return () => {
+			document.body.classList.remove("overflow-hidden");
+		};
+	}, []);
+
 	return ReactDOM.createPortal(
 		<div>
 			<div
 				onClick={handleClose}
-				className="absolute inset-0 bg-gray-300 opacity-80"
+				className="fixed inset-0 bg-gray-300 opacity-80"
 			></div>
-			<div className="absolute inset-40 p-10 bg-white">
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ut quos
-				ipsum reprehenderit facilis hic ratione!
+			<div className="fixed inset-40 p-10 bg-white">
+				<div className="flex flex-col justify-between h-full">
+					{children}
+					<div className="flex justify-end">{actionBar}</div>
+				</div>
 			</div>
 		</div>,
 		document.querySelector(".modal-container")
--- src/componets/pages/ModalPage.js
+++ src/componets/pages/ModalPage.js
@@ -11,12 +11,101 @@ export const ModalPage = () => {
 	const onClose = () => {
 		setShowModal((current) => !current);
 	};
+	const acceptTerms = (
+		<div>
+			<Button onClick={onClose} primary>
+				I Accept
+			</Button>
+		</div>
+	);
+
+	const modal = (
+		<Modal handleClose={onClose} actionBar={acceptTerms}>
+			<p>Here is an important agreement for you to accept</p>
+		</Modal>
+	);
+
 	return (
 		<div className="relative">
 			<Button onClick={handleClick} primary rounded>
 				Open Modal
 			</Button>
-			{showModal && <Modal handleClose={onClose} />}
+			{showModal && modal}
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia et
+				eveniet autem architecto praesentium tempora animi minus vitae nesciunt
+				deserunt.
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia et
+				eveniet autem architecto praesentium tempora animi minus vitae nesciunt
+				deserunt.
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia et
+				eveniet autem architecto praesentium tempora animi minus vitae nesciunt
+				deserunt.
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia et
+				eveniet autem architecto praesentium tempora animi minus vitae nesciunt
+				deserunt.
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia et
+				eveniet autem architecto praesentium tempora animi minus vitae nesciunt
+				deserunt.
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia et
+				eveniet autem architecto praesentium tempora animi minus vitae nesciunt
+				deserunt.
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia et
+				eveniet autem architecto praesentium tempora animi minus vitae nesciunt
+				deserunt.
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia et
+				eveniet autem architecto praesentium tempora animi minus vitae nesciunt
+				deserunt.
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia et
+				eveniet autem architecto praesentium tempora animi minus vitae nesciunt
+				deserunt.
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia et
+				eveniet autem architecto praesentium tempora animi minus vitae nesciunt
+				deserunt.
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia et
+				eveniet autem architecto praesentium tempora animi minus vitae nesciunt
+				deserunt.
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia et
+				eveniet autem architecto praesentium tempora animi minus vitae nesciunt
+				deserunt.
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia et
+				eveniet autem architecto praesentium tempora animi minus vitae nesciunt
+				deserunt.
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia et
+				eveniet autem architecto praesentium tempora animi minus vitae nesciunt
+				deserunt.
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia et
+				eveniet autem architecto praesentium tempora animi minus vitae nesciunt
+				deserunt.
+			</p>
 		</div>
 	);
 };
