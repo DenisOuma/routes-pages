6c3b1e4929eb1ea31638552f09c8253020411157
Denis Ouma <dennisouma38@gmail.com>
2023-04-01 10:43:35 +0300

    Adding the default colors for the tables

---
+++ src/componets/pages/TablesPage.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Table } from "../Table";
+
+export const TablesPage = () => {
+	const data = [
+		{
+			name: "Orange",
+			color: "bg-orange-500",
+			score: "5",
+		},
+		{
+			name: "Apple",
+			color: "bg-red-500",
+			score: "3",
+		},
+		{
+			name: "Banana",
+			color: "bg-yellow-500",
+			score: "1",
+		},
+		{
+			name: "Lime",
+			color: "bg-green-500",
+			score: "4",
+		},
+	];
+	return (
+		<div>
+			<Table data={data} />
+		</div>
+	);
+};
